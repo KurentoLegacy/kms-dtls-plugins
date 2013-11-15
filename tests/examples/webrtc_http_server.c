d5221f00328625fdab7e4be772be218523ade7d6
Miguel París Díaz <mparisdiaz@gmail.com>
2013-11-15 09:58:50 +0100

    Init media session
    Change-Id: Iab70cfe462f7f12af3446c4f6a1654b1dea441e9

--- tests/examples/webrtc_http_server.c
+++ tests/examples/webrtc_http_server.c
@@ -15,6 +15,7 @@
 
 #include <gst/gst.h>
 #include <libsoup/soup.h>
+#include <nice/nice.h>
 
 #define GST_CAT_DEFAULT webrtc_http_server
 GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
@@ -24,6 +25,71 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 #define PORT 8080
 #define MIME_TYPE "text/html"
 #define HTML_FILE "webrtc_loopback.html"
+
+typedef struct _MediaSession {
+  GMainContext *context;
+  GMainLoop * loop;
+
+  SoupServer *server;
+  SoupMessage *msg;
+
+  NiceAgent *agent;
+  guint stream_id;
+} MesiaSession;
+
+
+static void
+gathering_done (NiceAgent *agent, guint stream_id, MesiaSession *mediaSession)
+{
+  GST_WARNING ("TODO: implement");
+}
+
+static void
+nice_agent_recv (NiceAgent *agent, guint stream_id, guint component_id,
+                 guint len, gchar *buf, gpointer user_data)
+{
+  /* Nothing to do, this callback is only for negotiation */
+}
+
+static gpointer
+loop_thread (gpointer loop)
+{
+  g_main_loop_run (loop); /* TODO: g_main_loop_quit */
+
+  return NULL;
+}
+
+static void
+init_media_session (SoupServer *server, SoupMessage *msg)
+{
+  MesiaSession *mediaSession;
+  GThread * lt;
+
+  mediaSession = g_slice_new0 (MesiaSession); /* TODO: free */
+
+  mediaSession->context = g_main_context_new ();
+  mediaSession->loop = g_main_loop_new (mediaSession->context, TRUE); /* TODO: g_main_loop_unref */
+  lt = g_thread_new ("ctx", loop_thread, mediaSession->loop);
+  g_thread_unref (lt);
+
+  mediaSession->server = g_object_ref (server);
+  mediaSession->msg = g_object_ref (msg);
+
+  mediaSession->agent = nice_agent_new (mediaSession->context, NICE_COMPATIBILITY_RFC5245);
+  g_object_set (mediaSession->agent, "upnp", FALSE, NULL);
+  g_object_set(G_OBJECT (mediaSession->agent),
+               "stun-server", "77.72.174.167",
+               "stun-server-port", 3478,
+               NULL);
+
+  mediaSession->stream_id = nice_agent_add_stream (mediaSession->agent, 1);
+  nice_agent_attach_recv (mediaSession->agent, mediaSession->stream_id, 1, mediaSession->context,
+                         nice_agent_recv, NULL);
+  g_signal_connect (mediaSession->agent, "candidate-gathering-done",
+    G_CALLBACK (gathering_done), mediaSession);
+
+  nice_agent_gather_candidates (mediaSession->agent, mediaSession->stream_id);
+}
 
 static void
 server_callback (SoupServer *server, SoupMessage *msg, const char *path,
@@ -57,6 +123,8 @@ server_callback (SoupServer *server, SoupMessage *msg, const char *path,
 
   soup_message_set_response (msg, MIME_TYPE, SOUP_MEMORY_STATIC, "", 0);
   soup_message_body_append (msg->response_body, SOUP_MEMORY_TAKE, contents, length);
+
+  init_media_session (server, msg);
   GST_WARNING ("TODO: complete response");
 }
 
