669d4901432bc1b40827a431789e8b469bddbf6b
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-06-23 12:20:46 +0200

    Require gstreamer 1.3.3
    Change-Id: Ib1927e69a1ad6d8b87156a982526234081213eb2

--- config.h.cmake
+++ config.h.cmake
@@ -10,7 +10,4 @@
 /* The gettext domain name */
 #cmakedefine GETTEXT_PACKAGE "@GETTEXT_PACKAGE@"
 
-/* Gstreamer 1.3 or newer found */
-#cmakedefine GSTREAMER_1_3_FOUND "@GSTREAMER_1_3_FOUND@"
-
 #endif /* __KMS_DTLS_PLUGINS_CONFIG_H__ */
